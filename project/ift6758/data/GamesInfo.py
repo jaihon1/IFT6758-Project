fb548664ddb229b111934105e8a0645915ca3ce5
LaurenceM <laurencemarcotte@hotmail.ca>
2021-09-28 20:57:16 -0400

    Add init, load and download functions. None are implemented

--- project/ift6758/data/GamesInfo.py
+++ project/ift6758/data/GamesInfo.py
@@ -1,5 +1,31 @@
 import requests
 import json
+import os
+
 
 class GamesInfo:
-    pass
+    def __init__(self, season, filepath):
+        pass
+
+    def load(self, season, filepath):
+        """
+        Should load the json files for the corresponding season located at filepath.
+        Args:
+            season:
+            filepath:
+
+        Returns:
+
+        """
+        pass
+
+    def download(self, season, filepath):
+        """
+        Download and save the json files from corresponding season in the specified filepath.
+        Args:
+            season:
+
+        Returns:
+
+        """
+        pass
