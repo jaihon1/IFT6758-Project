fe4c6271d92d223ad1060986e3347da0a862d5ac
samibahig <samibahig@gmail.com>
2021-11-18 15:50:19 -0500

    changes of the Experience code

--- project/ift6758/data/Experience7.5.py
+++ project/ift6758/data/Experience7.5.py
@@ -3,11 +3,11 @@ import pandas as pd
 from comet_ml import Experiment
 
 #print(os.environ["KEY"])
-#experiment = Experiment(api_key=os.environ.get(COMET_API_KEY="mGWy9DnaNXu3caVMUCszMl6eP"),  project_name="milestone_2")
+experiment = Experiment(api_key=os.environ.get('COMET_API_KEY'), project_name="ift6758-project", workspace="jaihon")
 subset_df = pd.read_csv(r"C:\Users\samib\Desktop\ProjectIFT6758\IFT6758-Project\project\ift6758\data\games_data\games_data_all_seasons.csv", encoding='UTF-8')
 subset_df_WPG_WSH = subset_df[(subset_df['team_id'] == 'WSH') | (subset_df['team_id'] == 'WPG')]
 # Filter by March 12, 2018
 subset_df_WPG_WSH = subset_df_WPG_WSH[subset_df_WPG_WSH['datetime'].str.contains('2018-03-12')]
 
 experiment.log_dataframe_profile(subset_df_WPG_WSH, name='wpg_v_wsh_2017021065', dataframe_format='csv')
-# #exp.log_metrics({"auc": auc, “acc”: acc, “loss”: loss})as usual
+# #exp.log_metrics({"auc": auc, “acc”: acc, “loss”:Expercd  loss})as usual
