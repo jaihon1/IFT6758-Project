e1512f135da11d674684f908141161144de10c74
jaihon1 <jaihon.qasim@hotmail.com>
2021-11-19 14:42:17 -0500

    #21 adding more experiments...

--- project/ift6758/data/best_shot.py
+++ project/ift6758/data/best_shot.py
@@ -1,21 +1,33 @@
 #%%
-import numpy as np
-import keras
-from keras.callbacks import ModelCheckpoint
-import pandas as pd
-from sklearn.model_selection import train_test_split
+import os
+import joblib
 
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from comet_ml import Experiment
+
+# import keras
+from keras.callbacks import ModelCheckpoint
+from tensorflow import keras
+
+
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, roc_curve
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 #%%
 # Load the data
+# data = pd.read_csv("ift6758/data/games_data/games_data_all_seasons.csv")
 data = pd.read_csv("games_data/games_data_all_seasons.csv")
 
 
 # split into train and test
 data['game_pk'] = data['game_pk'].apply(lambda i: str(i))
-train, test = data[~data['game_pk'].str.startswith('2019')], data[data['game_pk'].str.startswith('2019')]
+train_data, test_data = data[~data['game_pk'].str.startswith('2019')], data[data['game_pk'].str.startswith('2019')]
 
 #%%
 # features = ['game_pk', 'side', 'event_index', 'event_type', 'shot_type',
@@ -66,10 +78,20 @@ def prep_data(data_train):
         # print(df_encoded.shape)
 
 
-    # TODO: add normalization/standardization to features
 
     # Split the data into features and labels for train and validation
     x_train, x_valid, y_train, y_valid = train_test_split(data.drop(columns=['is_goal']), data['is_goal'], test_size=0.2, stratify=data['is_goal'])
+
+    features_standardizing = [
+        'coordinate_x', 'coordinate_y',
+        'distance_net', 'angle_net', 'time_since_pp_started', 'current_time_seconds', 'current_friendly_on_ice', 'current_opposite_on_ice'
+    ]
+
+    # normalization/standardization to features
+    scaler = StandardScaler()
+    # scaler = MinMaxScaler()
+    x_train[features_standardizing] = scaler.fit_transform(x_train[features_standardizing])
+    x_valid[features_standardizing] = scaler.fit_transform(x_valid[features_standardizing])
 
     # print(data.shape)
     # print(x_train.shape)
@@ -77,10 +99,10 @@ def prep_data(data_train):
     # print(y_train.shape)
     # print(y_valid.shape)
 
-    return x_train, x_valid, y_train, y_valid
+    return x_train, x_valid, y_train, y_valid, selected_features
 
 #%%
-def build(x_train, x_valid, y_train, y_valid):
+def train_model(x_train, x_valid, y_train, y_valid):
 
     # Create the model
     model = keras.Sequential([
@@ -89,19 +111,20 @@ def build(x_train, x_valid, y_train, y_valid):
         keras.layers.Dense(1, activation='sigmoid')
     ])
 
+    opt = keras.optimizers.Adam(learning_rate=0.001)
+
     # Compile the model
-    model.compile(optimizer='adam',
+    model.compile(optimizer=opt,
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
 
-
     epoch = 10
 
-    filepath = 'my_best_model.epoch{epoch:02d}-loss{val_loss:.2f}.hdf5'
+    filepath = 'nn.epoch{epoch:02d}-loss{val_loss:.2f}.hdf5'
     checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     callbacks = [checkpoint]
 
-    class_weight = {0: 1., 1: 4.}
+    class_weight = {0: 1., 1: 2.}
 
     # Train the model
     model.fit(x_train, y_train, epochs=epoch, validation_data=(x_valid, y_valid), callbacks=callbacks, class_weight=class_weight)
@@ -109,22 +132,57 @@ def build(x_train, x_valid, y_train, y_valid):
     # Evaluate the model
     model.evaluate(x_valid, y_valid)
 
+    return model
 
+
+#%%
+def train_nn(x_train, x_valid, y_train, y_valid, features, comet=False):
+    if comet:
+        # Create experiment for comet
+        experiment = Experiment(
+            api_key=os.environ.get("COMET_API_KEY"),
+            project_name="ift6758-project",
+            workspace="jaihon",
+            auto_metric_logging=True,
+            auto_param_logging=True,
+            auto_histogram_weight_logging=True,
+            auto_histogram_gradient_logging=True,
+            auto_histogram_activation_logging=True
+        )
+        experiment.log_parameters({'model': 'nn', 'feature': features})
+
+    clf = train_model(x_train, x_valid, y_train, y_valid)
+
+
+    if comet:
+        model_name = 'nn'+'_'.join(features)
+        joblib.dump(clf, model_name+'.joblib')
+        experiment.log_model(model_name, model_name+'.joblib')
+
+    return clf
+#%%
+def plot_roc_curve(pred_prob, true_y, marker, label):
+    score = roc_auc_score(true_y, pred_prob)
+    fpr, tpr, _ = roc_curve(true_y, pred_prob)
+    sns.set_theme()
+    plt.grid(True)
+    plt.plot(fpr, tpr, linestyle=marker, label=label+f' (area={score:.2f})')
 #%%
 
 def main(data_train):
 
-    TRAIN = False
+    TOGGLE_TRAIN = False
 
-    x_train, x_valid, y_train, y_valid = prep_data(data_train)
+    x_train, x_valid, y_train, y_valid, features = prep_data(data_train)
 
-    if TRAIN:
-        build(x_train, x_valid, y_train, y_valid)
+
+    if TOGGLE_TRAIN:
+        clf = train_nn(x_train, x_valid, y_train, y_valid, features, comet=False)
 
 
     else:
         # File path
-        filepath = './my_best_model.epoch08-loss0.32.hdf5'
+        filepath = './nn.epoch05-loss0.28.hdf5'
 
         # Load the model
         model = keras.models.load_model(filepath, compile = True)
@@ -132,11 +190,11 @@ def main(data_train):
         # Generate predictions for samples
         predictions = model.predict(x_valid)
         # print(predictions)
-        # print(np.mean(predictions))
-        # print(np.std(predictions))
+        print(np.mean(predictions))
+        print(np.std(predictions))
 
 
-        threshold = 0.43
+        threshold = 0.28
         predictions[predictions <= threshold] = 0
         predictions[predictions > threshold] = 1
 
@@ -154,6 +212,14 @@ def main(data_train):
 
         # print(correct / len(predictions))
 
+        # ROC curve
+        plot_roc_curve(predictions, y_valid, '-', 'nn distance')
+        plt.xlabel('False positive rate')
+        plt.ylabel('True positive rate')
+        plt.legend()
+        plt.show()
+
+
 
 
 
@@ -162,5 +228,5 @@ def main(data_train):
 
 
 if __name__ == "__main__":
-    main(train)
+    main(train_data)
 # %%
