383a80f2691c21f18389a8d709f0de66c19bfbc8
LaurenceM <laurencemarcotte@hotmail.ca>
2021-11-22 12:39:29 -0500

    Created utils files for plot functions and to create experiments. Added generation of plot for default xgboost

--- project/ift6758/data/xgboost_models.py
+++ project/ift6758/data/xgboost_models.py
@@ -4,6 +4,8 @@ import xgboost as xgb
 from sklearn.model_selection import KFold, train_test_split
 from sklearn.metrics import confusion_matrix
 
+from project.ift6758.data.utils import plot_roc_curve, plot_goal_rate, plot_cumulative_sum, plot_calibration
+
 #%%
 random_state = np.random.RandomState(42)
 
@@ -18,7 +20,7 @@ train, test = data[~data['game_pk'].str.startswith('2019')], data[data['game_pk'
 
 #%%
 
-x_train, x_valid, y_train, y_valid = train_test_split(train[['distance_net', 'angle_net']], train['is_goal'], random_state=random_state)
+x_train, x_valid, y_train, y_valid = train_test_split(train[['distance_net', 'angle_net']], train['is_goal'], random_state=random_state, stratify=train['is_goal'])
 
 #%%
 model = xgb.XGBClassifier(n_jobs=1)
@@ -26,3 +28,15 @@ model = xgb.XGBClassifier(n_jobs=1)
 model.fit(x_train, y_train)
 pred = model.predict(x_valid)
 print(confusion_matrix(y_valid, pred))
+pred_proba = model.predict_proba(x_valid)
+
+accuracy = np.sum(y_valid == pred)/len(pred)
+
+#%%
+labels = ['default_XGBoost']
+probas = [pred_proba[:,1]]
+#%%
+plot_roc_curve(probas, y_valid, ['-'], labels)
+plot_goal_rate(probas, y_valid, labels)
+plot_cumulative_sum(probas, y_valid, labels)
+plot_calibration(probas, y_valid, labels)
