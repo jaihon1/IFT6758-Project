5ce637ec42d74712b7696c9e4158dd75e1947dfc
LaurenceM <laurencemarcotte@hotmail.ca>
2021-12-19 23:55:02 -0500

    Dashboard can now show sum of expected goals for each team and period time left and data used to compute predictions. Transition between models and game ids seem to be working.

--- app/ift6758/ift6758/game_client/GameClient.py
+++ app/ift6758/ift6758/game_client/GameClient.py
@@ -1,6 +1,4 @@
 import requests
-import json
-import logging
 
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
@@ -8,13 +6,20 @@ import pandas as pd
 
 from .EventGenerator import EventGenerator
 
-
-logger = logging.getLogger(__name__)
-
+import warnings
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 class GameClient: 
     def __init__(self):
         self.game_id = None
+        self.game_pk = None
+        self.home = None
+        self.away = None
+        self.game_type = None
+        self.dateTime = None
+        self.data = pd.DataFrame()
+        self.period = 0
+        self.time_left = "20:00"
         self.features = ['side', 'shot_type', 'period', 'period_type', 'coordinate_x', 'coordinate_y',
                          'team_side', 'distance_net', 'angle_net', 'previous_event_type', 'time_since_pp_started',
                          'previous_event_time_seconds', 'current_time_seconds', 'current_friendly_on_ice',
@@ -36,37 +41,38 @@ class GameClient:
         # Generate and save dataframe about specific features
         data = self.__download(game_id)
         if data is None:
-            logger.error("Unable to get the game data. Make sure the Game ID exists.")
             return pd.DataFrame()
-        logger.info("Game data successfully downloaded.")
         #file_path = '/home/johannplantin4/Documents/Dev/Code/IFT6758_group_project/IFT6758-Project/project/ift6758/data/games_data/2019/2019020900.json'
         # with open(file_path, 'r') as jaihon:
         #     data = json.load(jaihon)
-        if self.game_id != game_id:
+        if self.game_id != str(game_id):
             self.idx = 0
             self.game_pk = data['gamePk']
             self.home = data['gameData']['teams']['home']['triCode']
             self.away = data['gameData']['teams']['away']['triCode']
             self.game_type = data['gameData']['game']['type']
+            self.dateTime = data['gameData']['datetime']['dateTime']
         sides = dict()
-
         for period in data['liveData']['linescore']['periods']:
+            if self.period < period['num']:
+                self.period = period['num']
             sides[period['num']] = {self.home: period['home'].setdefault('rinkSide', np.NaN), self.away: period['away'].setdefault('rinkSide', np.NaN)}
         live_events = data['liveData']['plays']['allPlays']
         # if self.idx == 0:
         #     live_events = live_events[:len(live_events)//4]
         # else:
         #     live_events = live_events[:self.idx +len(live_events)//4]
-        if self.game_id != game_id:
+        if self.game_id != str(game_id):
             self.game = EventGenerator(self.game_pk, self.home, self.away, sides, live_events, self.game_type)
-            self.game_id = game_id
-        else:
-            self.game.live_events = live_events
-            self.game.sides = sides
+            self.game_id = str(game_id)
+
+        self.game.live_events = live_events
+        self.game.sides = sides
+
         dataframe = self.game.build(self.idx)
         self.idx = len(self.game.live_events)
-
         if len(dataframe) != 0:
+            self.time_left = self.game.live_events[-1]['about']['periodTimeRemaining']
             final_df = self.__add_new_features(dataframe)
             final_df = final_df[self.features]
 
@@ -75,16 +81,19 @@ class GameClient:
             final_df = self.__encode_categorical(final_df)
 
             final_df['Rebound'] = final_df['Rebound'].astype('bool')
+            if self.game_id != str(game_id):
+                self.data = final_df
+            else:
+                self.data.append(final_df)
         else:
             final_df = pd.DataFrame()
 
         return final_df
 
     def __download(self, game_id):
-        print('Downloading game:', game_id)
 
         # get the general information of all the regular and playoff games from the season
-        url_game = f'https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/'
+        url_game = f'https://statsapi.web.nhl.com/api/v1/game/{int(game_id)}/feed/live/'
 
         r = requests.get(url_game)
 
